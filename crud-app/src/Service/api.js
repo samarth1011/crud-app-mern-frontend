1410d45a7b48b533e8d8dbe4d42f8e9f46c7f95f
samarth1011 <samarth.mailme@gmail.com>
2022-12-20 17:33:50 +0530

    wee

--- crud-app/src/Service/api.js
+++ crud-app/src/Service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const usersUrl = 'http://localhost:3003/users';
-const usersUrl = 'https://crud-app-mern.vercel.app/';
+const usersUrl = 'https://crud-app-mern.vercel.app';
 
 export const getUsers = async (id) => {
     id = id || '';
